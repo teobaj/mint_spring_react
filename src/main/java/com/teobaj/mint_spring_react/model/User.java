d3585cb2912cd2b1f07434ed5696e0ee6827943f
teobaj <teobaj2309@gmail.com>
2020-09-09 23:44:27 +0300

    Basic Start

---
+++ src/main/java/com/teobaj/mint_spring_react/model/User.java
@@ -0,0 +1,48 @@
+package com.teobaj.mint_spring_react.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.validation.constraints.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value={"createdAt", "updatedAt"}, allowGetters = true)
+public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    
+    @NotBlank
+    private String name;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Min(value = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+
+}
