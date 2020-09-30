e4a31f6f9ee7a6dfaf40dac9ae8932d532203a46
teobaj <teobaj2309@gmail.com>
2020-09-30 19:18:41 +0300

    Get mapping annotations

--- src/main/java/com/teobaj/mint_spring_react/controller/UserController.java
+++ src/main/java/com/teobaj/mint_spring_react/controller/UserController.java
@@ -2,6 +2,7 @@ package com.teobaj.mint_spring_react.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api")
