0e9c2f5a55f6e960f0d6502edd7a7cc52e5ab044
teobaj <teobaj2309@gmail.com>
2020-09-30 19:16:00 +0300

    get user retrun startement

--- src/main/java/com/teobaj/mint_spring_react/controller/UserController.java
+++ src/main/java/com/teobaj/mint_spring_react/controller/UserController.java
@@ -6,5 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class UserController {
-    
+    @GetMapping("/user")
+    public String getUser(){
+        return "salut"
+    }
 }
